88e91a4349353a796a33756e02c47975b0a8c3cc
sifath islam <sifathislam790@gmail.com>
2024-05-21 11:42:35 +0600

    [fix] add workspace name

--- workspaces/urls.py
+++ workspaces/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api/search-member/', search_member, name='search-member'),
     path('userWorkspace/<int:workspace_id>/user/<int:user_id>/is_member/', IsUserMember.as_view(), name='is-user-member'),
+    path('singleWorkspace/<int:pk>/', WorkSpaceDetailView.as_view(), name='workspace_detail'),
     path('user/<int:user_id>/workspaces/', UserWorkspaces.as_view(), name='user-workspaces'),
     path('singleworkspace/<int:workspace_id>/members/', WorkspaceMembersList.as_view(), name='workspace-members'), path('workspace/<int:workspace_id>/members/', WorkspaceMembersList.as_view(), name='workspace-members'),
     path('singleworkspace/<int:workspace_id>/timelines/', WorkspaceTimelinesList.as_view(), name='workspace-timelines'),
