e75c7b4fb75c713f6f0a4f5da4d445c933f02096
sifath islam <sifathislam790@gmail.com>
2024-05-23 00:06:38 +0600

    [ADD & FIX] Add and fix apis

--- workspaces/serializers.py
+++ workspaces/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
 from accounts.serializers import UserRegisterSerializer
+from datetime import date
 
 # * ================ This Serializer is for the WorkSpace Creation ================ * #
 class WorkSpaceSerializer(serializers.ModelSerializer):
@@ -8,16 +9,50 @@ class WorkSpaceSerializer(serializers.ModelSerializer):
         model = WorkSpace
         fields = '__all__'
 
-
 # * ================ This Serializer is for the Timeline Creation ================ * #
-class TimeLineSerializer(serializers.ModelSerializer):
+class TimelineCreationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timeline
-        fields = ['name','assign','details','start_Date','end_Date','duration','comment','status']
-    def validate_assign(self, value):
-        if value.role != roles_choice.TEAM_LEADER:
-            raise serializers.ValidationError("Assigned member must be a Team Leader")
-        return value
+        fields = ['workspace_Name','name', 'details', 'start_Date', 'end_Date']
+        read_only_fields = ['id', 'assign', 'remaining_time', 'duration', 'status']
+
+    def validate(self, attrs):
+        # Validate start and end dates
+        if attrs.get('start_Date') and attrs.get('end_Date'):
+            if attrs['start_Date'] > attrs['end_Date']:
+                raise serializers.ValidationError("Start date cannot be after end date.")
+        return attrs
+
+    def create(self, validated_data):
+        # Automatically set the status to "To Do"
+        validated_data['status'] = Timeline_Status.TO_DO
+        return super().create(validated_data)
+
+# * ================ This Serializer is for the Get the Timeline  ================ * #
+class AssignedUserSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(source='user.first_name', read_only=True)
+    last_name = serializers.CharField(source='user.last_name', read_only=True)
+    email = serializers.EmailField(source='user.email', read_only=True)
+
+    class Meta:
+        model = Member
+        fields = ['id', 'first_name', 'last_name', 'email']
+
+class TimelineDetailSerializer(serializers.ModelSerializer):
+    workspace_name = serializers.CharField(source='workspace_Name.name', read_only=True)
+    assign = AssignedUserSerializer(read_only=True)
+    remaining_time = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Timeline
+        fields = ['id', 'name', 'details', 'start_Date', 'end_Date', 'workspace_name', 'assign', 'remaining_time', 'duration', 'status']
+
+    def get_remaining_time(self, obj):
+        if obj.end_Date:
+            remaining_days = (obj.end_Date - date.today()).days
+            return remaining_days if remaining_days >= 0 else 0
+        return None
+
 
 # * ================ This Serializer is for the Member Search ================ * #
 class MemberSerializer(serializers.ModelSerializer):
@@ -33,6 +68,7 @@ class MemberSerializerForRoleFind(serializers.ModelSerializer):
     class Meta: 
         model = Member
         fields = ['role']
+
 
 # * ================ This Serializer is for the Scrum ================ * #
 class ScrumSerializer(serializers.ModelSerializer):
--- workspaces/urls.py
+++ workspaces/urls.py
@@ -8,7 +8,6 @@ router = routers.DefaultRouter()
 
 router.register('list' , workSpaceViewSet)
 router.register('member' , MemberViewSet)
-router.register('create/timeline' , TimelineViewSet)
 router.register('create/scrum' , scrumViewset)
 router.register('create/task' , taskViewset)
 router.register('create/taskcomments' , taskCommentViewset)
@@ -16,18 +15,26 @@ router.register('create/taskcomments' , taskCommentViewset)
 urlpatterns = [
     path('', include(router.urls)),
     path('api/search-member/', search_member, name='search-member'),
-
+# * ================= Work space URLS ================================
     path('userWorkspace/<int:workspace_id>/user/<int:user_id>/is_member/', IsUserMember.as_view(), name='is-user-member'),
     path('singleWorkspace/<int:pk>/', WorkSpaceDetailView.as_view(), name='workspace_detail'),
     path('user/<int:user_id>/workspaces/', UserWorkspaces.as_view(), name='user-workspaces'),
     path('user/<int:user_id>/workspace/<int:workspace_id>/position/', user_position_in_workspace, name='user-position-in-workspace'),
-
+    
+# * ================= Members URLS ================================
     path('singleworkspace/<int:workspace_id>/members/', WorkspaceMembersList.as_view(), name='workspace-members'), 
     path('workspace/<int:workspace_id>/members/', WorkspaceMembersList.as_view(), name='workspace-members'),
     
+# * ================= Timeline URLS ================================
+    path('timelines/create/', TimelineCreateView.as_view(), name='timeline-create'),
+    path('timelines/<int:pk>/', TimelineDetailView.as_view(), name='timeline-detail'),
+    path('timelines/update/<int:pk>/', TimelineUpdateView.as_view(), name='timeline-update'),
+    path('timelines/delete/<int:pk>/', TimelineDeleteView.as_view(), name='timeline-delete'),
     path('singleworkspace/<int:workspace_id>/timelines/list/', WorkspaceTimelinesList.as_view(), name='workspace-timelines'),
+
+
+# * ================= Scrum URLS ================================
     path('singletimeline/<int:timeline_id>/scrums/list/', timeline_scrums, name='timeline-scrums'),
-    
     path('singlescrum/<int:scrum_id>/tasks/list/', scrum_tasks, name='scrum-tasks'),
     path('task/<int:pk>/priority/update/', TaskPriorityUpdateView.as_view(), name='task-priority-update'),
     path('task/<int:pk>/status/update/', TaskPriorityUpdateView.as_view(), name='task-priority-update'),
